cfa493ac75ece38909500938192125a19ce49232
JosueOb <josuericardocando@hotmail.com>
2019-01-22 20:09:24 -0500

    Validación de login

--- app/Controllers/AccessController.php
+++ app/Controllers/AccessController.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 use App\Models\User;
 
+//Se define esta clase para acceder a la sistema
 class AccessController extends BaseController{
 
     public function __construct(){
@@ -19,28 +20,42 @@ class AccessController extends BaseController{
     }
     public function postLogin($request){
         $postData = $request->getParsedBody();
+        $userEmail = $postData['userEmail'];
+        $userPass = $postData['userPass'];
         $responseMessage = null;
-        $user = User::where('userEmail',$postData['userEmail'])->first();
-        if($user){
-            if(\password_verify($postData['userPass'], $user->userPassword)){
-                $_SESSION['user']= $user->getAttributes();
-                // var_dump($user->getAttributes());
-                if($user->userType == 'Admin'){
-                    return $this->redirectResponse('/admin');
-                    
-                }elseif($user->userType == 'User'){
-                    $responseMessage= 'User';
-                    // return $this->redirectResponse('/user');
+        
+        if(!empty($userEmail) && !empty($userPass)){
+            if(filter_var($userEmail, FILTER_VALIDATE_EMAIL)){
+                $user = User::where('userEmail',$userEmail)->first();
+                if($user){
+                    if(\password_verify($userPass, $user->userPassword)){
+                        $_SESSION['user']= $user->getAttributes();
+                        // var_dump($user->getAttributes());
+                        if($user->userType == 'Admin'){
+                            return $this->redirectResponse('/admin');
+                            
+                        }elseif($user->userType == 'User'){
+                            $responseMessage= 'User';
+                            // return $this->redirectResponse('/user');
+                        }
+                        
+                    }else{
+                        $responseMessage='Datos Incorrectos';
+                    }
+                }else{
+                    $responseMessage='Datos Incorrectos';
                 }
-                
             }else{
-                $responseMessage='Datos Incorrectos';
+                $responseMessage = 'Formato del correo incorrecto';
             }
         }else{
-            $responseMessage='Datos Incorrectos';
+            $responseMessage = 'Ingrese todos los campos';
         }
+
+        // var_dump($errorsMesssage);
+        // die;
         return $this->renderHTML('login.twig',[
-            'responseMessage'=>$responseMessage
+            'responseMessage'=>$responseMessage,
         ]);
 
     }
